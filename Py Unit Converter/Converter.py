2b348131551da30452742c677db5b4a047d439c2
edwinblack <30639180+edwinblack@users.noreply.github.com>
2019-06-22 00:47:15 -0600

    Firt Commit

---
+++ Py Unit Converter/Converter.py
@@ -0,0 +1,44 @@
+from Distance import *
+
+
+def Convert_Speed(self, parameter_list):
+    try:
+        pass
+    except Exception as e:
+        pass
+
+def Convert_Coordinates(self, parameter_list):
+    try:
+        pass
+    except Exception as e:
+        pass
+
+def Convert_Volume(self, parameter_list):
+    try:
+        pass
+    except Exception as e:
+        pass
+
+def Convert_Pressure(self, parameter_list):
+    try:
+        pass
+    except Exception as e:
+        pass
+
+def Convert_Distance(self, parameter_list):
+    try:
+        pass
+    except Exception as e:
+        pass
+
+def Convert_Consumption(self, parameter_list):
+    try:
+        pass
+    except Exception as e:
+        pass
+
+def Convert_Temperature(self, parameter_list):
+    try:
+        pass
+    except Exception as e:
+        pass
---
+++ Py Unit Converter/Speed/Speed_Converter.py
@@ -0,0 +1,6 @@
+
+def ConvertToMetersFromKilometers(self, value):
+    try:
+        pass
+    except Exception as e:
+        pass
