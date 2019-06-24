fb64d18f44dd7dd07be62093d89d934c3eee2d86
Edwin_PC <ejjcifuentes209929@gmail.com>
2019-06-24 03:55:06 -0600

    working  pressure

--- Py Unit Converter/Pressure/Pressure_Converter.py
+++ Py Unit Converter/Pressure/Pressure_Converter.py
@@ -1,2 +1,38 @@
 
 # Convert any to KPA
+def ConvertToKPAFromPSI(self, value):
+    try:
+        return value * 6.895
+    except Exception as e:
+        return "Error Converting: " + e
+
+def ConvertToKPAFromMMHG(self, value):
+    try:
+        return value / 7.501
+    except Exception as e:
+        return "Error Converting: " + e
+
+def ConvertToKPAFromBAR(self, value):
+    try:
+        return value * 100
+    except Exception as e:
+        return "Error Converting: " + e
+
+# Convert any to MMHG
+def ConvertToKPAFromPSI(self, value):
+    try:
+        return value * 6.895
+    except Exception as e:
+        return "Error Converting: " + e
+
+def ConvertToKPAFromMMHG(self, value):
+    try:
+        return value / 7.501
+    except Exception as e:
+        return "Error Converting: " + e
+
+def ConvertToKPAFromBAR(self, value):
+    try:
+        return value * 100
+    except Exception as e:
+        return "Error Converting: " + e
